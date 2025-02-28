752ed694580e04550a3034c0278d690adf15e306
Bobi <wavelet-exabyte0j@icloud.com>
2025-02-28 08:36:05 +0100

    Delete databank.sql

--- databank.sql
+++
@@ -1,8 +0,0 @@
-CREATE DATABASE bilder_grid;
-USE bilder_grid;
-
-CREATE TABLE bilder (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    feldnummer INT UNIQUE NOT NULL,
-    bildpfad VARCHAR(255) NOT NULL
-);
